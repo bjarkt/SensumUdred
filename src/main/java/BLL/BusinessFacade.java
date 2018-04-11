0a4a9d2eead5bb5cd5dc4937431ec0f1a5574e79
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2018-04-11 13:44:42 +0200

    JUnit added.

--- src/main/java/BLL/BusinessFacade.java
+++ src/main/java/BLL/BusinessFacade.java
@@ -5,7 +5,6 @@ import DAL.IPersistent;
 public class BusinessFacade implements IBusiness {
 	private IPersistent persistent;
 
-
 	@Override
 	public void injectPersistent(IPersistent persistent) {
 		this.persistent = persistent;
