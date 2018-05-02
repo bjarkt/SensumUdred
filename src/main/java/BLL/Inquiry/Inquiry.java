a4c22a2281004ceb7b560f5f29475cadf2c878c4
Lasat17 <Lasat17@student.sdu.dk>
2018-05-02 09:07:59 +0200

    Made som changes in the inquiry class

--- src/main/java/BLL/Inquiry/Inquiry.java
+++ src/main/java/BLL/Inquiry/Inquiry.java
@@ -1,6 +1,38 @@
 package BLL.Inquiry;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Inquiry {
 
-    
+    private String CPR;
+    private String name;
+    private String address;
+    private String description;
+    private String gender;
+    private String birthDate;
+    private String civilStatus;
+
+
+
+
+
+
+
+private void editInquiry(){
+    File file = new File ("main/java/BLL/DATA/");
+
+    try {
+        Scanner scanner = new Scanner(file);
+
+        PrintStream writer = new PrintStream(file);
+
+
+
+    } catch (FileNotFoundException ex) {
+        System.out.println("RIP");
+    }
+}
 }
