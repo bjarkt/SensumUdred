85ba0cf77ab4d51854f964ce5b01c9de6624d62b
Lasat17 <Lasat17@student.sdu.dk>
2018-05-02 09:43:42 +0200

    Made changes in inquiry

--- src/main/java/BLL/Inquiry/Inquiry.java
+++ src/main/java/BLL/Inquiry/Inquiry.java
@@ -15,6 +15,64 @@ public class Inquiry {
     private String birthDate;
     private String civilStatus;
 
+    public String getCPR() {
+        return CPR;
+    }
+
+    public void setCPR(String CPR) {
+        this.CPR = CPR;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCivilStatus() {
+        return civilStatus;
+    }
+
+    public void setCivilStatus(String civilStatus) {
+        this.civilStatus = civilStatus;
+    }
+
+
+
 
 
 
@@ -25,16 +83,16 @@ private void saveInquiry(){
     File file = new File ("main/java/BLL/DATA/" + CPR + ".txt");
 
     try {
-        Scanner scanner = new Scanner(file);
+
 
         PrintStream writer = new PrintStream(file);
-        writer.println(CPR);
-        writer.println(name);
-        writer.println(address);
-        writer.println(description);
-        writer.println(gender);
-        writer.println(birthDate);
-        writer.println(civilStatus);
+        writer.println(CPR + "\n");
+        writer.println(name + "\n");
+        writer.println(address + "\n");
+        writer.println(description + "\n");
+        writer.println(gender + "\n");
+        writer.println(birthDate + "\n");
+        writer.println(civilStatus + "\n");
 
 
     } catch (FileNotFoundException ex) {
@@ -42,6 +100,27 @@ private void saveInquiry(){
     }
 }
 private void editInquiry(){
+
+
+    File file = new File ("main/java/BLL/DATA/" + CPR + ".txt");
+
+    try {
+        Scanner scanner = new Scanner(file);
+        
+
+
+
+
+
+
+
+
+
+    } catch (FileNotFoundException exception) {
+        System.out.println("RIP");
+    }
+
+    }
 
 }
 }
