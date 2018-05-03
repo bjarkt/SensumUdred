af74c07c189141e2a74e858b3fa4ac6b9987eeb4
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-03 17:23:26 +0200

    comments for httprequestutil

--- src/main/java/DAL/http_request_utility/HttpRequestUtility.java
+++ src/main/java/DAL/http_request_utility/HttpRequestUtility.java
@@ -8,6 +8,15 @@ import java.net.URL;
 import java.util.Map;
 
 public class HttpRequestUtility {
+    /**
+     * Make an HTTP request to the URL
+     * @param urlString Which url to request
+     * @param query Request queries, as a map
+     * @param method Which method to use (POST/GET/etc..)
+     * @param acceptType What do you except the server will output? text/pdf/etc...
+     * @return byte array of output from server
+     * @throws IOException
+     */
     public static byte[] makeHttpRequest(String urlString, Map<String, String> query, HttpMethod method, HttpAcceptType acceptType) throws IOException {
         StringBuilder sb = new StringBuilder();
         URL url = new URL(urlString);
@@ -51,6 +60,13 @@ public class HttpRequestUtility {
         return data;
     }
 
+    /**
+     * Read binary data from the inputstream
+     * @param inputStream inputstream from the {@link HttpURLConnection}
+     * @param contentLength how many bytes is the content?
+     * @return byte array of read data
+     * @throws IOException
+     */
     private static byte[] readBinary(InputStream inputStream, int contentLength) throws IOException {
         byte[] buffer = new byte[1024];
         ByteArrayOutputStream data = new ByteArrayOutputStream(contentLength);
@@ -63,6 +79,12 @@ public class HttpRequestUtility {
         return data.toByteArray();
     }
 
+    /**
+     * Read text data from an input stream
+     * @param inputStream inputstream from the {@link HttpURLConnection}
+     * @return string as a byte array
+     * @throws IOException
+     */
     private static byte[] readText(InputStream inputStream) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder sb = new StringBuilder();
@@ -75,6 +97,11 @@ public class HttpRequestUtility {
         return sb.toString().getBytes();
     }
 
+    /**
+     * Transform a query map into a query string
+     * @param query map of query key value pairs
+     * @return string of query key value pairs
+     */
     private static String queryMapToString(Map<String, String> query) {
         StringBuilder sb = new StringBuilder();
 
