bdc49ad273a6be18d17acdb6a2f5340bed171d04
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-05-04 09:20:50 +0200

    Removed unused imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,5 +19,6 @@ public class Main {
 
         ui.startApplication(args);
 
+
     }
 }
--- src/main/java/UI/components/drawer/DrawerController.java
+++ src/main/java/UI/components/drawer/DrawerController.java
@@ -1,17 +1,8 @@
 package UI.components.drawer;
 
 import UI.components.Component;
-import UI.components.IEventListener;
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXTextField;
-import com.jfoenix.validation.RequiredFieldValidator;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class DrawerController extends Component implements IDrawer {
