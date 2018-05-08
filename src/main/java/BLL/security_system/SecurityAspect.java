8d773f328e3f602af98aca23c8e6932e8e08d1ed
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-05-08 09:37:25 +0200

    Removing all the sout's from the aspect code

--- src/main/java/BLL/security_system/SecurityAspect.java
+++ src/main/java/BLL/security_system/SecurityAspect.java
@@ -1,18 +1,25 @@
 package BLL.security_system;
 
+import BLL.ACQ.IUser;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.SuppressAjWarnings;
 import org.aspectj.lang.reflect.MethodSignature;
 
 import java.lang.reflect.Method;
 
 @Aspect
+@SuppressAjWarnings("adviceDidNotMatch")
 public class SecurityAspect {
+	private static int userLevel;
+
+	public static void setUserLevel(IUser user) {
+		userLevel = user.getAccessLevel().ordinal();
+	}
 
 	@Around("@annotation(SecurityLevel) && execution(* *(..))")
 	public Object securityLogic(ProceedingJoinPoint joinPoint) throws Throwable {
-		//Default Object that we can use to return to the consumer
 		Object returnObject;
 
 		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
@@ -22,14 +29,9 @@ public class SecurityAspect {
 
 		int securityLevel = annotation.value();
 
-		System.out.println(securityLevel);
-
-		if(10 >= securityLevel) {
-			System.out.println("User has access to the method.");
-
+		if(userLevel >= securityLevel) {
 			returnObject = joinPoint.proceed();
 		} else {
-
 			throw new SecurityException("User does not have access to the method.");
 		}
 
