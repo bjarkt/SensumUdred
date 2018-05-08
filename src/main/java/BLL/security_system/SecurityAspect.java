dc01db78809f35e2124ae71bd179f70ba64970ca
Dennis Petersen <Dennis-drengen@hotmail.dk>
2018-05-08 12:39:05 +0200

    JavaDoc and small changes to the security system

--- src/main/java/BLL/security_system/SecurityAspect.java
+++ src/main/java/BLL/security_system/SecurityAspect.java
@@ -8,18 +8,31 @@ import org.aspectj.lang.annotation.SuppressAjWarnings;
 import org.aspectj.lang.reflect.MethodSignature;
 
 import java.lang.reflect.Method;
-
+/**
+ * Contains the logic for the security system.
+ * It is an Aspect from the AspectJ library.
+ */
 @Aspect
 @SuppressAjWarnings("adviceDidNotMatch")
-public class SecurityAspect {
+public abstract class SecurityAspect {
 	private static int userLevel;
 
-	public static void setUserLevel(IUser user) {
+	/**
+	 * Sets the user for the security system.
+	 * @param user any user
+	 */
+	static void setUserLevel(IUser user) {
 		userLevel = user.getAccessLevel().ordinal();
 	}
 
+	/**
+	 * A logic that runs every time a method with the {@link SecurityLevel} annotation.
+	 * @param joinPoint a joinpoint
+	 * @return the method
+	 * @throws Throwable throws a {@link SecurityException} if user does not have access
+	 */
 	@Around("@annotation(SecurityLevel) && execution(* *(..))")
-	public Object securityLogic(ProceedingJoinPoint joinPoint) throws Throwable {
+	private Object securityLogic(ProceedingJoinPoint joinPoint) throws Throwable {
 		Object returnObject;
 
 		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
@@ -32,7 +45,7 @@ public class SecurityAspect {
 		if(userLevel >= securityLevel) {
 			returnObject = joinPoint.proceed();
 		} else {
-			throw new SecurityException("User does not have access to the method.");
+			throw new SecurityException("User does not have the privilege to this method .");
 		}
 
 		return returnObject;
