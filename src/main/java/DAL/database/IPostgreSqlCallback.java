900bc1299a9f0e58eac8eb0955e5d5fb53e69f80
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-11 13:20:42 +0200

    fix merge error

--- src/main/java/DAL/database/IPostgreSqlCallback.java
+++ src/main/java/DAL/database/IPostgreSqlCallback.java
@@ -4,9 +4,5 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IPostgreSqlCallback {
-<<<<<<< HEAD
-	void execute(Connection connection) throws SQLException;
-=======
 	void execute(Connection conn) throws SQLException;
->>>>>>> b82d0e4d0f636bee8fc33138dc502429832673ce
 }
