8e26161707f8c2cabfa419a8d257a1ffb018fdc9
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-14 18:07:08 +0200

    use municipal in csv, instead of city area

--- src/test/java/ZipCodeUtilityTest.java
+++ src/test/java/ZipCodeUtilityTest.java
@@ -7,10 +7,10 @@ public class ZipCodeUtilityTest {
 
     @Test
     public void ZipCodeTest() {
-        assertEquals("Esbjerg N", ZipCodeUtility.getInstance().getMunicipality(6715));
-        assertEquals("Odense S", ZipCodeUtility.getInstance().getMunicipality(5260));
+        assertEquals("Esbjerg", ZipCodeUtility.getInstance().getMunicipality(6715));
+        assertEquals("Odense", ZipCodeUtility.getInstance().getMunicipality(5260));
         assertEquals("Frederiksberg", ZipCodeUtility.getInstance().getMunicipality(2000));
-        assertEquals("København Ø", ZipCodeUtility.getInstance().getMunicipality(2100));
+        assertEquals("København", ZipCodeUtility.getInstance().getMunicipality(2100));
 
         assertNull(ZipCodeUtility.getInstance().getMunicipality(6716));
 
