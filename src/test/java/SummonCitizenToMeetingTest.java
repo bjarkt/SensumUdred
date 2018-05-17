029001dd653efeb6cdf7f800c47cc15c2f3db730
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-17 11:01:11 +0200

    fix meetingTest

--- src/test/java/SummonCitizenToMeetingTest.java
+++ src/test/java/SummonCitizenToMeetingTest.java
@@ -103,7 +103,7 @@ public class SummonCitizenToMeetingTest {
 
             @Override
             public String getSocialSecurityNumber() {
-                return null;
+                return ssn;
             }
         };
     }
