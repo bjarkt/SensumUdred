fd2f81e0391ee90c0f3b87f27916e0b240d024c7
migigenDK <migigenDK@gmail.com>
2018-05-17 10:57:48 +0200

    Revert "Revert "Revert "redirect"""
    This reverts commit 0ed3a26fdd490a6089898ee8d289b06da1753358.

--- src/main/java/BLL/redirect_citizens_to_other_department/RedirectCitizen.java
+++ src/main/java/BLL/redirect_citizens_to_other_department/RedirectCitizen.java
@@ -1,30 +1,4 @@
 package BLL.redirect_citizens_to_other_department;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class RedirectCitizen {
-
-    private boolean Approved;
-    private String  CPR;
-
-
-            public static void main(String args[]) {
-
-                String fileName = "main/java/DATA/\" + test + \".txt";
-
-                try (Scanner scanner = new Scanner(new File(fileName))) {
-
-                    while (scanner.hasNext()){
-                        System.out.println(scanner.nextLine());
-                    }
-
-                } catch (IOException e) {
-                    System.out.println("file not found");
-                    e.printStackTrace();
-                    }
-        }
-
-    }
-
+}
