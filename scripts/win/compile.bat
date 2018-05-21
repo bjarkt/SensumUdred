58234a115da5e83375d973de8a58e2e8c8a31e4e
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-21 17:02:49 +0200

    update compile scripts to latest jfoenix jar

--- scripts/win/compile.bat
+++ scripts/win/compile.bat
@@ -14,4 +14,4 @@ xcopy src\main\resources out\yee /E /K > nul
 xcopy lib out\yee /E /K > nul
 
 xcopy users.txt out\yee /K > nul
-java -jar lib\aspectj\aspectjtools.jar -d out\yee -source 1.8 -sourceroots src\main\java\ -cp "lib/jfoenix-8.0.1.jar;lib/aspectj/aspectjrt.jar;lib/gson-2.8.4.jar;out/yee/" %*
+java -jar lib\aspectj\aspectjtools.jar -d out\yee -source 1.8 -sourceroots src\main\java\ -cp "lib/jfoenix-8.0.4.jar;lib/aspectj/aspectjrt.jar;lib/gson-2.8.4.jar;out/yee/" %*
