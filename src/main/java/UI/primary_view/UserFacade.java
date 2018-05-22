a41223273c447c1aaed74a186875b72bb69219da
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2018-05-22 14:39:20 +0200

    Updated login method in GUI to match new tings.

--- src/main/java/UI/primary_view/UserFacade.java
+++ src/main/java/UI/primary_view/UserFacade.java
@@ -238,8 +238,8 @@ public class UserFacade implements IUserInterface, Initializable {
 
 	private void setupUpLoginView(){
 		logInView.onLogIn(data -> {
-			if(business.getSigningService().signIn(data[0], data[1]) != null){
-				user = business.getSigningService().signIn(data[0], data[1]);
+			user = business.getSigningService().signIn(data[0], data[1]);
+			if(user != null){
 				SecuredAspect.setUser(user);
 				isLoggedIn.setValue(true);
 			} else{
