90994f2b080d7fc308148971a0c4aa11678524d7
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-23 12:37:52 +0200

    update compile

--- scripts/win/compile.bat
+++ scripts/win/compile.bat
@@ -13,4 +13,4 @@ del /S out\yee\*.java > nul
 xcopy src\main\resources out\yee /E /K > nul
 xcopy lib out\yee /E /K > nul
 
-java -jar lib\aspectj\aspectjtools.jar -d out\yee -source 1.8 -sourceroots src\main\java\ -cp "lib/jfoenix-8.0.4.jar;lib/aspectj/aspectjrt.jar;lib/gson-2.8.4.jar;lib/jbcrypt-0.4.jar;out/yee/" %*
+java -jar lib\aspectj\aspectjtools.jar -d out\yee -source 1.8 -sourceroots src\main\java\ -encoding utf-8 -cp "lib/jfoenix-8.0.4.jar;lib/aspectj/aspectjrt.jar;lib/gson-2.8.4.jar;lib/jbcrypt-0.4.jar;out/yee/" %*
