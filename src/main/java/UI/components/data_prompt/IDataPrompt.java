57e784c84b8a5f9f48cfe6569a780a5571c296a5
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2018-05-23 16:10:26 +0200

    "Ny henvendelse" flow is now being implemented in GUI. New "DataPrompt" component can be used to dynamically set textfields, reading values and proceeding to next page of textfields.

--- src/main/java/UI/components/data_prompt/IDataPrompt.java
+++ src/main/java/UI/components/data_prompt/IDataPrompt.java
@@ -2,9 +2,16 @@ package UI.components.data_prompt;
 
 import UI.components.IComponent;
 import ACQ.IEventListener;
+import UI.components.textfield.ITextField;
+
+import java.util.List;
 
 public interface IDataPrompt extends IComponent {
 
+    void onContinue(IEventListener<List<String>> listener);
 
+    void setPrompt(String prompt);
+
+    void addTextFields(String ...field);
 
 }
