c8f4fbc57d1f940a6a58aa6227be0845469d89fd
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-24 23:11:14 +0200

    Update task

--- src/main/java/UI/task/Task.java
+++ src/main/java/UI/task/Task.java
@@ -1,25 +1,41 @@
 package UI.task;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Task<T> {
     private ExecutorService executor;
     private CompletableFuture<T> myData;
-    private CallbackWithoutData runningCallback;
+    private BooleanProperty running;
 
-    public Task(Supplier<T> data, CallbackWithoutData runningCallback) {
+    public Task(Supplier<T> data) {
         executor = Executors.newFixedThreadPool(1);
 
-        runningCallback.action();
+        running = new SimpleBooleanProperty(true);
         myData = CompletableFuture.supplyAsync(data, executor);
     }
 
     public void setOnSucceeded(CallbackWithData<T> callback) {
-        myData.thenAccept(callback::action);
-        executor.shutdown();
+         myData.thenAcceptAsync(new Consumer<T>() {
+            @Override
+            public void accept(T t) {
+                callback.action(t);
+                running.setValue(false);
+                executor.shutdown();
+            }
+        });
+    }
+
+    public void setFinishedListener(ChangeListener<Boolean> changeListener) {
+        running.addListener(changeListener);
     }
 }
 
