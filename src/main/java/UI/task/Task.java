2c1904dc5e0d60f0ee8994a5c4601edb4236341a
bjarkt <bjarke.tobiesen@gmail.com>
2018-05-24 23:28:12 +0200

    Remove finished property from Task

--- src/main/java/UI/task/Task.java
+++ src/main/java/UI/task/Task.java
@@ -1,9 +1,5 @@
 package UI.task;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,12 +9,10 @@ import java.util.function.Supplier;
 public class Task<T> {
     private ExecutorService executor;
     private CompletableFuture<T> myData;
-    private BooleanProperty finished;
 
     public Task(Supplier<T> data) {
         executor = Executors.newFixedThreadPool(1);
 
-        finished = new SimpleBooleanProperty(false);
         myData = CompletableFuture.supplyAsync(data, executor);
     }
 
@@ -27,14 +21,9 @@ public class Task<T> {
             @Override
             public void accept(T t) {
                 callback.action(t);
-                finished.setValue(true);
                 executor.shutdown();
             }
         });
-    }
-
-    public void setFinishedListener(ChangeListener<Boolean> changeListener) {
-        finished.addListener(changeListener);
     }
 }
 
